72f7aeda4e01fc969ab58bc0485eff10c63aad16
IgorWAndrade <33557880+IgorWAndrade@users.noreply.github.com>
2020-10-02 07:18:22 -0300

    Resolução do Exercicio 03 de Metodos

--- Exercicios/Metodos/Exercicio03/index01.js
+++ Exercicios/Metodos/Exercicio03/index01.js
@@ -2,3 +2,51 @@
 //     03.01
 //         Criar uma função que irá receber um número digitado pelo usuario e irá imprimir em ordem crescente
 //     o número até 100. (OBS: Validar se número digitado é menor que 100 e impedir proseguir enquanto não for valor correto)
+
+// 01 - Propriedades
+const readLineSync = require('readline-sync');
+let numero = 0;
+
+
+// 02 - Funcoes
+
+function retornarNumeroDigitadoPeloUsuario() {
+    return readLineSync.question('Digite um numero menor que 100: ');
+}
+
+function validarNumeroDigitadoPeloUsuarioERetornarConvertido(numeroParametro) {
+
+    let valorInvalido = true;
+
+    do {
+
+        if (isNaN(numeroParametro)) {
+            numeroParametro = retornarNumeroDigitadoPeloUsuario();
+        } else {
+            numeroParametro = parseInt(numeroParametro);
+        }
+
+        if (numeroParametro < 100) {
+            valorInvalido = false;
+        } else {
+            numeroParametro = retornarNumeroDigitadoPeloUsuario();
+        }
+
+    } while (valorInvalido == true);
+
+    return numeroParametro;
+}
+
+function imprimirEmOrdemCrescente(numeroParametro) {
+
+    for (let i = 0; i <= numeroParametro; i++) {
+        console.log(i);
+    }
+
+}
+
+// 03 - Resolução
+
+numero = retornarNumeroDigitadoPeloUsuario();
+numero = validarNumeroDigitadoPeloUsuarioERetornarConvertido(numero);
+imprimirEmOrdemCrescente(numero);
--- Exercicios/Metodos/Exercicio03/index02.js
+++ Exercicios/Metodos/Exercicio03/index02.js
@@ -2,3 +2,51 @@
 //     03.02
 //         Criar uma função que irá receber um número digitado pelo usuario e irá imprimir em ordem decrescente
 //     o número até 0. (OBS: Validar se número digitado é maior que 0 e impedir proseguir enquanto não for valor correto)
+
+// 01 - Propriedades
+const readLineSync = require('readline-sync');
+let numero = 0;
+
+
+// 02 - Funcoes
+
+function retornarNumeroDigitadoPeloUsuario() {
+    return readLineSync.question('Digite um numero maior que 0: ');
+}
+
+function validarNumeroDigitadoPeloUsuarioERetornarConvertido(numeroParametro) {
+
+    let valorInvalido = true;
+
+    do {
+
+        if (isNaN(numeroParametro)) {
+            numeroParametro = retornarNumeroDigitadoPeloUsuario();
+        } else {
+            numeroParametro = parseInt(numeroParametro);
+        }
+
+        if (numeroParametro > 0) {
+            valorInvalido = false;
+        } else {
+            numeroParametro = retornarNumeroDigitadoPeloUsuario();
+        }
+
+    } while (valorInvalido == true);
+
+    return numeroParametro;
+}
+
+function imprimirEmOrdemDecrescente(numeroParametro) {
+
+    for (let i = numeroParametro; i >= 0; i--) {
+        console.log(i);
+    }
+
+}
+
+// 03 - Resolução
+
+numero = retornarNumeroDigitadoPeloUsuario();
+numero = validarNumeroDigitadoPeloUsuarioERetornarConvertido(numero);
+imprimirEmOrdemDecrescente(numero);
